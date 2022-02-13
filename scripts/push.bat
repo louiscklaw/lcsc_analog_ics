47fedd5f58e216f2758ab61e6ace4435ebd96d61
louiscklaw <logickee@gmail.com>
2022-02-13 14:17:32 +0800

    update lcsc_analog_ics,

--- scripts/push.bat
+++ scripts/push.bat
@@ -1,6 +1,11 @@
 git add . 
-git commit -m"update,"
+git commit -m"update lcsc_analog_ics,"
 
 git pull
 
 git push
+
+cd ..
+  git pull origin master
+  git commit -m"update lcsc_analog_ics,"
+cd lcsc_analog_ics
